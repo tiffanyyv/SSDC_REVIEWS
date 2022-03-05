a600b7a11d9ffcf3b174e3b58f5f7636f11c3838
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-03-04 23:49:28 -0800

    Added count property and fixed sorting property

--- database/queries/getReviews.js
+++ database/queries/getReviews.js
@@ -11,15 +11,18 @@ const getReviewsQuery = async (product_id, count, page, sort) => {
     sort = `ORDER BY helpfulness DESC, reviews.review_date DESC`;
   }
 
-
+// TODO: format date
   const results = await pool.query(
     `SELECT product_id AS product,
     (SELECT ${page} AS page),
     (SELECT ${count} AS count),
-      (SELECT array_agg
-        (json_build_object('review_id', review_id, 'rating', rating, 'summary', summary, 'recommend', recommend, 'response', response, 'body', body, 'date', review_date, 'reviewer_name', reviewer_name, 'helpfulness', helpfulness, 'photos', (SELECT coalesce(array_agg(json_build_object('id', photo_id, 'url', photo_url)), '{}') AS photos FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id)
-      ) ${sort})
-        AS results from reviews WHERE product_id = $1)
+      (WITH limitrevs AS (SELECT * FROM reviews WHERE product_id = $1 ${sort} LIMIT ${count})
+      SELECT array_agg
+        (json_build_object('review_id', review_id, 'rating', rating, 'summary', summary, 'recommend', recommend, 'response', response, 'body', body, 'date', review_date, 'reviewer_name', reviewer_name, 'helpfulness', helpfulness, 'photos',
+          (SELECT coalesce(array_agg(json_build_object('id', photo_id, 'url', photo_url)), '{}')
+          AS photos FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id)
+        ))
+         AS results from limitrevs)
     FROM reviews WHERE product_id = $1`
     , [product_id])
   // results is an array of objects
