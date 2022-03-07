754c8f526d428f6449c52a93742e84946721ed45
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-03-06 23:52:45 -0800

    Add photos working

--- database/queries/postReview.js
+++ database/queries/postReview.js
@@ -11,26 +11,23 @@ const postReviewQuery = async (
     photos
   }, currentUnixDate) => {
 
-  // const addPhotos = (photos) => {
-  //   photos.map(photo => {
-  //     return photo.url;
-  //   })
-  // }
 
   const results = await pool.query(
     `INSERT INTO reviews
     (product_id, rating, review_date, summary, body, recommend, reviewer_name, reviewer_email)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING review_id`
-    // `WITH results as (
-    //   INSERT INTO reviews (product_id, rating, review_date, summary, body, recommend, reviewer_name, reviewer_email)
-    //   VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING review_id
-    // )
-    // INSERT INTO reviews_photos (review_id, photo_url) `
   , [product_id, rating, currentUnixDate, summary, body, recommend, name, email])
   .then(async ({ rows }) => {
       let review_id = rows[0].review_id;
       try {
-        // if ()
+        if (photos.length > 0) {
+          await photos.forEach(photo => {
+            pool.query(`INSERT into reviews_photos (review_id, photo_url) VALUES ($1, $2)`, [review_id, photo])
+          })
+        }
+      }
+      catch (err) {
+        console.log(err)
       }
     }
   )
