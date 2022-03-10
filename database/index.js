5bc30e51fc666932563337b89b213e2c90085a5e
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-03-09 19:59:54 -0800

    Add pool pw

--- database/index.js
+++ database/index.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 5432,
   user: process.env.USER || 'postgres',
+  password: process.env.PASSWORD || '',
   database: process.env.DATABASE || 'reviews'
 })
 
