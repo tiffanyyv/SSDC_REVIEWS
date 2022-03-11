59810615be3147f293fba1a05e6818e7e994b40e
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-03-10 22:15:35 -0800

    Fixed index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
